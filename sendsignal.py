bdc520c0ea76d04b8f2688610c412919467c50be
caitlyn.powers <caitlyn@oneil-beta.com>
2024-04-24 13:41:06 -0700

    vv

--- sendsignal.py
+++ sendsignal.py
@@ -18,8 +18,6 @@ import json
     operation: Everything is hardcoded since this script was just made for testing. 
 """
 
-HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
 
 
